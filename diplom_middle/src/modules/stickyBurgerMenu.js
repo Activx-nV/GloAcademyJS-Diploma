37aef848f0ca0d90401efa365209f2ce434ac2d0
Lauri K <laurikamm@gmail.com>
2021-01-16 20:12:23 +0200

    feat: make burger menu functional

--- diplom_middle/src/modules/stickyBurgerMenu.js
+++ diplom_middle/src/modules/stickyBurgerMenu.js
@@ -1,9 +1,25 @@
 const stickyBurgerMenu = () => {
     const topMenu = document.querySelector('.top-menu');
-    const menuBtn = document.querySelector('.menu-button');
+    const popUpMenu = document.querySelector('.popup-menu');
+    const closeBtn = document.querySelector('.close-menu-btn');
+    topMenu.addEventListener('click', event => {
+        if (event.target.tagName === "IMG") {
+            popUpMenu.style.display = 'flex';
+        }
+    });
+    closeBtn.addEventListener('click', event => {
+        if (event.target.tagName === "IMG") {
+            popUpMenu.style.display = 'none';
+        }
+    });
+    popUpMenu.addEventListener('click', event => {
+        if (event.target.tagName === "A") {
+            popUpMenu.style.display = 'none';
+        }
+    });
 
     topMenu.style.overflow = 'hidden';
-    const sticky = topMenu.offsetTop;
+    const sticky = topMenu.offsetTop + 40;
     function stickyHandler() {
         if (window.pageYOffset >= sticky) {
             topMenu.classList.add("sticky");
